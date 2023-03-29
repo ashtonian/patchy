ead79043a4a1a137aafd5e08c378a2dfb2c26f71
Ashton Kinslow <github@ashtonkinslow.com>
2023-03-29 00:02:31 -0500

    add updates to metadata crawler

--- patchy.go
+++ patchy.go
@@ -1,7 +1,9 @@
 package patchy
 
 import (
+	"errors"
 	"reflect"
+	"strconv"
 	"strings"
 	"unicode"
 )
@@ -15,6 +17,7 @@ type Op struct {
 
 type EntityID interface{}
 
+type ColumnNameFunc func(field reflect.Type) string
 type ValidatorFunc func(value interface{}) error
 type ConverterFunc func(value interface{}) (interface{}, error)
 type OptionFunc func(*Patchy)
@@ -22,22 +25,32 @@ type OptionFunc func(*Patchy)
 type Patchy struct {
 	entityType  reflect.Type
 	tableName   string
-	columnNames []string
+	colNameFunc ColumnNameFunc
 	validator   ValidatorFunc
 	converter   ConverterFunc
-	allowedOps  map[string]bool
-	// fieldInfoCache map[string]fieldInfo
 }
 
-func NewPatchy(entityType reflect.Type, options ...OptionFunc) *Patchy {
+func NewPatchy(entityType reflect.Type, options ...OptionFunc) (*Patchy, error) {
+	if entityType.Kind() == reflect.Ptr {
+		entityType = entityType.Elem()
+	}
+
+	if entityType.Kind() != reflect.Struct {
+		return nil, errors.New("input type should be a struct or a pointer to a struct")
+	}
+
 	p := &Patchy{
 		entityType: entityType,
-		tableName:  entityType.Name(),
+		tableName:  ToSnakeCase(entityType.Name()),
+		colNameFunc: func(field reflect.Type) string {
+			// TODO: parse db tag
+			return ToSnakeCase(field.Name())
+		},
 	}
 	for _, option := range options {
 		option(p)
 	}
-	return p
+	return p, nil
 }
 
 func WithValidator(validator ValidatorFunc) OptionFunc {
@@ -52,6 +65,12 @@ func WithConverter(converter ConverterFunc) OptionFunc {
 	}
 }
 
+func WithColumnNameFunc(colNameFunc ColumnNameFunc) OptionFunc {
+	return func(p *Patchy) {
+		p.colNameFunc = colNameFunc
+	}
+}
+
 func WithTableName(tableName string) OptionFunc {
 	return func(p *Patchy) {
 		p.tableName = tableName
@@ -61,42 +80,6 @@ func WithTableName(tableName string) OptionFunc {
 var (
 	TagName = "patchy"
 )
-
-// func NewPatchSpec(model interface{}) (*PatchSpec, error) {
-// 	if model == nil {
-// 		return nil, errors.New("patchy: 'Model' is a required field")
-// 	}
-
-// 	t := reflect.TypeOf(model)
-// 	if t.Kind() != reflect.Struct {
-// 		return nil, errors.New("patchy: 'Model' must be a struct type")
-// 	}
-// 	fields := list.New()
-// 	for i := 0; i < t.NumField(); i++ {
-// 		l.PushFront(t.Field(i))
-// 	}
-// 	for l.Len() > 0 {
-// 		f := l.Remove(l.Front()).(reflect.StructField)
-// 		_, ok := f.Tag.Lookup(TagName)
-// 		// TODO: loop through fields check types create spec, enable field nesting
-
-// 	}
-// 	return nil, nil
-// }
-
-// created using reflection once, then used every query to generate sql.
-type PatchSpec struct {
-	TargetResource string
-	Fields         []PatchField
-}
-
-type PatchField struct {
-	AllowedOps string
-	TargetType string
-	// Validator func // only need two funcs
-	// Converter func
-	// Filter func
-}
 
 func (op *Op) GetValue() interface{} {
 	return nil
@@ -122,6 +105,134 @@ func ToSnakeCase(s string) string {
 			result.WriteRune(r)
 		}
 	}
-
 	return result.String()
+}
+
+type FieldMetadata struct {
+	Type            reflect.Kind
+	IsSlice         bool
+	IsMap           bool
+	IsStruct        bool
+	IsPrimitive     bool
+	SliceElemType   reflect.Kind
+	MapValueType    reflect.Kind
+	StructFieldName string
+	AllowedOps      []string
+	IsIgnored       bool
+	ColumnName      string
+}
+
+// func (p *Patchy) GetFieldMetadata(jsonPointer string) (FieldMetadata, error) {
+
+// 	parts := strings.Split(jsonPointer, "/")
+// 	if len(parts) < 2 {
+// 		return FieldMetadata{}, errors.New("invalid JSON pointer format")
+// 	}
+
+// 	return p.getFieldMetadataRec(t, parts[1:])
+// }
+
+func (p *Patchy) getFieldMetadataRec(t reflect.Type, parts []string) (FieldMetadata, error) {
+	if len(parts) == 0 {
+		return FieldMetadata{}, nil
+	}
+
+	fieldName, field, err := getFieldByJsonTag(t, parts[0])
+	if err != nil {
+		return FieldMetadata{}, err
+	}
+
+	if len(parts) == 1 {
+		return p.buildMetadata(field, fieldName), nil
+	}
+
+	switch field.Kind() {
+	case reflect.Struct:
+		return p.getFieldMetadataRec(field, parts[1:])
+	case reflect.Ptr:
+		return p.getFieldMetadataRec(field.Elem(), parts[1:])
+	case reflect.Slice:
+		index, err := parseArrayIndex(parts[1])
+		if err != nil {
+			return FieldMetadata{}, err
+		}
+
+		if index == "-" {
+			return p.buildMetadata(field.Elem(), fieldName), nil
+		}
+
+		return p.getFieldMetadataRec(field.Elem(), parts[2:])
+	case reflect.Map:
+		return p.getFieldMetadataRec(field.Elem(), parts[1:])
+	default:
+		return FieldMetadata{}, errors.New("invalid JSON pointer")
+	}
+}
+
+// getFieldByJsonTag returns the field name and type of the field with the given json tag.
+func getFieldByJsonTag(t reflect.Type, tagName string) (string, reflect.Type, error) {
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		jsonTag := field.Tag.Get("json")
+		if strings.Split(jsonTag, ",")[0] == tagName {
+			return field.Name, field.Type, nil
+		}
+	}
+
+	return "", nil, errors.New("field not found")
+}
+
+func (p *Patchy) buildMetadata(field reflect.Type, fieldName string) FieldMetadata {
+	meta := FieldMetadata{
+		ColumnName:      p.colNameFunc(field),
+		IsSlice:         false,
+		IsMap:           false,
+		IsStruct:        false,
+		IsPrimitive:     false,
+		SliceElemType:   reflect.Invalid,
+		MapValueType:    reflect.Invalid,
+		StructFieldName: fieldName,
+	}
+
+	switch field.Kind() {
+	case reflect.Slice:
+		meta.IsSlice = true
+		meta.SliceElemType = field.Elem().Kind()
+	case reflect.Map:
+		meta.IsMap = true
+		meta.MapValueType = field.Elem().Kind()
+	case reflect.Struct:
+		meta.IsStruct = true
+	case reflect.Ptr:
+		meta.Type = field.Elem().Kind()
+		meta.IsPrimitive = isPrimitiveType(field.Elem().Kind())
+	default:
+		meta.Type = field.Kind()
+		meta.IsPrimitive = isPrimitiveType(field.Kind())
+	}
+
+	return meta
+
+}
+
+func isPrimitiveType(k reflect.Kind) bool {
+	switch k {
+	case reflect.Bool, reflect.String, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Float32, reflect.Float64:
+		return true
+	default:
+		return false
+	}
+}
+
+func parseArrayIndex(s string) (string, error) {
+	if s == "-" {
+		return s, nil
+	}
+	_, err := strconv.Atoi(s)
+	if err != nil {
+		return "", errors.New("invalid array index")
+	}
+
+	return s, nil
+
 }
--- patchy_test.go
+++ patchy_test.go
@@ -1,6 +1,9 @@
 package patchy
 
 import (
+	"errors"
+	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -22,4 +25,193 @@ func TestToSnakeCase(t *testing.T) {
 			t.Errorf("toSnakeCase(%q) = %q; want %q", tt.input, got, tt.want)
 		}
 	}
+}
+
+func TestGetFieldMetadata(t *testing.T) {
+	type Address struct {
+		Street string `json:"street"`
+		City   string `json:"city"`
+	}
+
+	type Person struct {
+		Name    string            `json:"name"`
+		Age     int               `json:"age"`
+		Address *Address          `json:"address"`
+		Hobbies []string          `json:"hobbies"`
+		Pets    map[string]string `json:"pets"`
+	}
+
+	patchy, err := NewPatchy(reflect.TypeOf(Person{}))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	type TestCase struct {
+		name          string
+		pointer       string
+		expectedValue *FieldMetadata
+		expectedError error
+	}
+
+	testCases := []TestCase{
+		{
+			name:    "regular field",
+			pointer: "/name",
+			expectedValue: &FieldMetadata{
+				Type:          reflect.String,
+				IsSlice:       false,
+				IsMap:         false,
+				IsStruct:      false,
+				IsPrimitive:   true,
+				SliceElemType: reflect.Invalid,
+				MapValueType:  reflect.Invalid,
+				// Tags:            "json:\"name\"",
+				StructFieldName: "Name",
+			},
+			expectedError: nil,
+		},
+		{
+			name:    "embedded field",
+			pointer: "/street",
+			expectedValue: &FieldMetadata{
+				// Type:          reflect.TypeOf(*person).Kind(),
+				IsSlice:       false,
+				IsMap:         false,
+				IsStruct:      false,
+				IsPrimitive:   true,
+				SliceElemType: reflect.Invalid,
+				MapValueType:  reflect.Invalid,
+				// Tags:            "json:\"street\"",
+				StructFieldName: "Street",
+			},
+			expectedError: nil,
+		},
+		{
+			name:    "slice index",
+			pointer: "/hobbies/0",
+			expectedValue: &FieldMetadata{
+				Type:          reflect.TypeOf([]string{}).Kind(),
+				IsSlice:       true,
+				IsMap:         false,
+				IsStruct:      false,
+				IsPrimitive:   true,
+				SliceElemType: reflect.TypeOf("").Kind(),
+				MapValueType:  reflect.Invalid,
+				// Tags:            "json:\"hobbies\"",
+				StructFieldName: "Hobbies",
+			},
+			expectedError: nil,
+		},
+		{
+			name:    "map key",
+			pointer: "/pets/dog",
+			expectedValue: &FieldMetadata{
+				Type:          reflect.TypeOf(map[string]string{}).Kind(),
+				IsSlice:       false,
+				IsMap:         true,
+				IsStruct:      false,
+				IsPrimitive:   true,
+				SliceElemType: reflect.Invalid,
+				MapValueType:  reflect.Invalid,
+				// Tags:            "json:\"pets\"",
+				StructFieldName: "Pets",
+			},
+			expectedError: nil,
+		},
+		{
+			name:    "root",
+			pointer: "",
+			expectedValue: &FieldMetadata{
+				// Type:          reflect.TypeOf(*person).Kind(),
+				IsSlice:       false,
+				IsMap:         false,
+				IsStruct:      true,
+				IsPrimitive:   false,
+				SliceElemType: reflect.Invalid,
+				MapValueType:  reflect.Invalid,
+				// Tags:            "",
+				StructFieldName: "",
+			},
+			expectedError: nil,
+		},
+		{
+			name:    "invalid pointer format",
+			pointer: "invalid",
+		},
+		{
+			name:          "nonexistent field",
+			pointer:       "/nonexistent",
+			expectedValue: nil,
+			expectedError: errors.New("field not found: nonexistent"),
+		},
+		{
+			name:          "slice index out of range",
+			pointer:       "/hobbies/100",
+			expectedValue: nil,
+			expectedError: errors.New("index out of range: 100"),
+		},
+		{
+			name:          "invalid slice index",
+			pointer:       "/hobbies/invalid",
+			expectedValue: nil,
+			expectedError: errors.New("invalid index: invalid"),
+		},
+		{
+			name:          "invalid map key",
+			pointer:       "/pets/invalid",
+			expectedValue: nil,
+			expectedError: errors.New("invalid key: invalid"),
+		},
+		{
+			name:    "nil pointer",
+			pointer: "/address/city",
+			expectedValue: &FieldMetadata{
+				Type:          reflect.String,
+				IsSlice:       false,
+				IsMap:         false,
+				IsStruct:      false,
+				IsPrimitive:   true,
+				SliceElemType: reflect.Invalid,
+				MapValueType:  reflect.Invalid,
+				// Tags:            "json:\"city\"",
+				StructFieldName: "City",
+			},
+			expectedError: nil,
+		},
+	}
+	for _, tc := range testCases {
+		actualValue, actualError := patchy.getFieldMetadataRec(patchy.entityType, strings.Split(tc.pointer, "/")[1:])
+		if actualValue.Type != tc.expectedValue.Type {
+			t.Errorf("%s: expected type %v, actual type %v", tc.name, tc.expectedValue.Type, actualValue.Type)
+		}
+		if actualValue.IsSlice != tc.expectedValue.IsSlice {
+			t.Errorf("%s: expected IsSlice %t, actual IsSlice %t", tc.name, tc.expectedValue.IsSlice, actualValue.IsSlice)
+		}
+		if actualValue.IsMap != tc.expectedValue.IsMap {
+			t.Errorf("%s: expected IsMap %t, actual IsMap %t", tc.name, tc.expectedValue.IsMap, actualValue.IsMap)
+		}
+		if actualValue.IsStruct != tc.expectedValue.IsStruct {
+			t.Errorf("%s: expected IsStruct %t, actual IsStruct %t", tc.name, tc.expectedValue.IsStruct, actualValue.IsStruct)
+		}
+		if actualValue.IsPrimitive != tc.expectedValue.IsPrimitive {
+			t.Errorf("%s: expected IsPrimitive %t, actual IsPrimitive %t", tc.name, tc.expectedValue.IsPrimitive, actualValue.IsPrimitive)
+		}
+		if actualValue.SliceElemType != tc.expectedValue.SliceElemType {
+			t.Errorf("%s: expected SliceElemType %v, actual SliceElemType %v", tc.name, tc.expectedValue.SliceElemType, actualValue.SliceElemType)
+		}
+		if actualValue.MapValueType != tc.expectedValue.MapValueType {
+			t.Errorf("%s: expected MapValueType %v, actual MapValueType %v", tc.name, tc.expectedValue.MapValueType, actualValue.MapValueType)
+		}
+		// if actualValue.Tags != tc.expectedValue.Tags {
+		// 	t.Errorf("%s: expected Tags %s, actual Tags %s", tc.name, tc.expectedValue.Tags, actualValue.Tags)
+		// }
+		if actualValue.StructFieldName != tc.expectedValue.StructFieldName {
+			t.Errorf("%s: expected StructFieldName %s, actual StructFieldName %s", tc.name, tc.expectedValue.StructFieldName, actualValue.StructFieldName)
+		}
+
+		if (actualError != nil && tc.expectedError == nil) || (actualError == nil && tc.expectedError != nil) || (actualError != nil && tc.expectedError != nil && actualError.Error() != tc.expectedError.Error()) {
+			t.Errorf("%s: expected error %v, actual error %v", tc.name, tc.expectedError, actualError)
+		}
+	}
+
 }
